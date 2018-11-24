970a2a696db57dfb768a19a31926d31c8e4862fa
denis.korepin <denis.korepin@accenture.com>
2018-11-24 18:14:23 +0300

    Разворачиваемая панель

--- src/components/Modules/Home/Home.tsx
+++ src/components/Modules/Home/Home.tsx
@@ -1,9 +1,46 @@
 import * as React from 'react';
+import {ExpandingPanel} from '../../Core/ExpandingPanel';
+import {EExpandingPanelType} from '../../Core/Enums';
 
 export const Home = () => {
     return (
-        <div>
-            Домашняя страница
+        <div className="row">
+            <div className="col-xs-12">
+                <ExpandingPanel
+                    collapsed
+                    className="mt-2 col-xs-4"
+                    headerClassName="pb-0"
+                    collapsedIconClassName="fa-plus"
+                    expandedIconClassName="fa-minus"
+                    panelType={EExpandingPanelType.EXPANDING_BLOCK}
+                    header="Раскрой меня"
+                >
+                    Домашняя страница
+                </ExpandingPanel>
+                <ExpandingPanel
+                    collapsed
+                    className="mt-2 col-xs-4"
+                    headerClassName="pb-0"
+                    collapsedIconClassName="fa-plus"
+                    expandedIconClassName="fa-minus"
+                    panelType={EExpandingPanelType.EXPANDING_PANEL}
+                    header="Раскрой меня"
+                >
+                    Домашняя страница
+                </ExpandingPanel>
+                <ExpandingPanel
+                    collapsed
+                    className="mt-2 col-xs-4"
+                    headerClassName="pb-0"
+                    collapsedIconClassName="fa-plus"
+                    expandedIconClassName="fa-minus"
+                    panelType={EExpandingPanelType.SPOILER}
+                    header="Раскрой меня"
+                >
+                    Домашняя страница
+                </ExpandingPanel>
+        
+            </div>
         </div>
     )
 }
