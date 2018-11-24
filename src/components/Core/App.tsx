dad66f9dd9ac35096aaaef75dd65952d281f877c
codyfet <codyfet@mail.ru>
2018-11-25 00:15:45 +0300

    Добавлена функциональность логин (с рестом)

--- src/components/Core/App.tsx
+++ src/components/Core/App.tsx
@@ -6,16 +6,27 @@ const Chatkit = require('@pusher/chatkit');
 
 import {Login} from '../Modules/Login/Login';
 import {LoginData} from '../../models/Common';
+import { login } from '../../services/services';
 
 interface IProps {
 
 }
 
+/**
+ * @prop {object} chatkitUser Chatkit пользователь.
+ * @prop {boolean} loginView Признак отрисовки экрана логин.
+ * @prop {boolean} loginViewError Признак ошибки выполнения запроса на логин в нашу систему.
+ * @prop {LoginData} loginData Данные с формы логина.
+ * @prop {boolean} isLoading Признак загрузки.
+ * @prop {Array} rooms Комнаты пользователя.
+ */
 interface IState {
     chatkitUser: object;
     loginView: boolean;
+    loginViewError: boolean;
     loginData: LoginData;
     isLoading: boolean;
+    rooms: Array<number>;
 }
 
 export class App extends React.Component<IProps, IState> {
@@ -26,16 +37,14 @@ export class App extends React.Component<IProps, IState> {
         this.state = {
             chatkitUser: null,
             loginView: true,
+            loginViewError: false,
             loginData: null,
-            isLoading: false
+            isLoading: false,
+            rooms: [19385475]
         }
     }
 
     connectToChat () {
-        this.setState({
-            isLoading: true
-        });
-
         const chatManager = new Chatkit.ChatManager({
             instanceLocator: 'v1:us1:2847dca1-27c1-4716-a4bd-fe2089a67d6f',
             userId: this.state.loginData.username,
@@ -49,9 +58,6 @@ export class App extends React.Component<IProps, IState> {
                 this.setState({
                     chatkitUser: currentUser
                 });
-
-                console.log('currentUser');
-                console.log(currentUser);
 
                 // Подключаемся к комнате.
                 // currentUser.subscribeToRoom({
@@ -87,9 +93,6 @@ export class App extends React.Component<IProps, IState> {
                 if (error.statusCode === 404) {
                     alert('Такого пользователя не существует');
                 }
-                this.setState({
-                    isLoading: false
-                });
             })
     }
 
@@ -102,11 +105,33 @@ export class App extends React.Component<IProps, IState> {
     //     })
     // }
 
+    login (loginData: LoginData) {
+        // Логинимся в нашу систему.
+        login(loginData).then(
+            (response: any) => {
+                if (response.data.authentication) {
+                    this.setState({
+                        loginView: false
+                    });
+                    // Коннектимся к chatkit ui.
+                    this.connectToChat();
+                } else {
+                    this.setState({
+                        loginViewError: true
+                    });
+                }
+            },
+            (error: any) => {
+                console.log('login error');
+                console.log(error);
+            }
+        );
+    }
+
     handleEnterClick = (loginData: LoginData) => {
         this.setState({
-            loginView: false,
             loginData
-        }, () => this.connectToChat())
+        }, () => this.login(loginData))
     }
 
     render () {
@@ -116,10 +141,15 @@ export class App extends React.Component<IProps, IState> {
                     <Header />
                     <Main />
                 </React.Fragment>
-            )
+            );
         }
 
-        return <Login onEnterClick={this.handleEnterClick} />;
+        return (
+            <React.Fragment>
+                <Login onEnterClick={this.handleEnterClick} />
+                {this.state.loginViewError && <span>Неправильный логин или пароль!</span>}
+            </React.Fragment>
+        );
     }    
 
 }
--- src/services/services.ts
+++ src/services/services.ts
@@ -1,6 +1,15 @@
+import { LoginData } from "../models/Common";
+
 const axios = require('axios');
 
 axios.defaults.baseURL = 'http://10.124.0.7:3000';
+
+/**
+ * Функция логин.
+ */
+export function login (loginData: LoginData) {
+    return axios.post("/users/login", {"enterpriseId": loginData.username, "password": loginData.password});
+}
 
 /**
  * Получить список всех пользователей.
