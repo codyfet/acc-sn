dc6b0d9f351e9738d0ff74fa4a0c0c7265bbb82d
codyfet <codyfet@mail.ru>
2018-11-24 16:31:31 +0300

    Обновил ридми

--- webpack.config.js
+++ webpack.config.js
@@ -40,13 +40,5 @@ module.exports = {
         // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: [".less", ".ts", ".tsx", ".js", ".json"]
     },
-    plugins: [htmlPlugin],
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    // externals: {
-    //     "react": "React",
-    //     "react-dom": "ReactDOM"
-    // }
+    plugins: [htmlPlugin]
 };
